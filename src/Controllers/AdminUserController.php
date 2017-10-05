96900b881d24420c4e7a8817508447e2e12f57c1
soltm <mariusz.soltys@suffolk.gov.uk>
2017-10-05 16:18:24 +0100

    WIP - initial

---
+++ src/Controllers/AdminUserController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *
+ * Project: uservel
+ * Date: 05/10/2017
+ * @author Mariusz Soltys.
+ * @version 1.0.0
+ * @license All Rights Reserved
+ *
+ */
+
+namespace marsoltys\Controllers;
+
+
+use Illuminate\Routing\Controller;
+
+class AdminUserController extends Controller
+{
+
+}
---
+++ src/UservelServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *
+ * Project: uservel
+ * Date: 05/10/2017
+ * @author Mariusz Soltys.
+ * @version 1.0.0
+ * @license All Rights Reserved
+ *
+ */
+
+namespace marsoltys\uservel;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class UservelServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+
+}
